0e670ac5845cf92937f41ac88e7057f1c52be030
Matt Makai <makaimc@gmail.com>
2015-01-25 17:24:26 -0500

    adding example msg

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def twilio_response():
     response = twiml.Response()
     # enter your SMS response message below
-    msg = "Thank you! Email me at makai@twilio.com with questions."
+    msg = """Thank you for your feedback! Check out http://bit.ly/1rRQ7mS
+          for more information on Python!"""
     response.message(msg)
     return Response(response.toxml(), mimetype="text/xml")
 
